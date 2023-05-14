7bf71aae5e331cd1fbf1c3c0542e43928ad8c3e6
Riad REKAB <hr_rekab@esi.dz>
2023-05-14 18:57:23 +0200

    Added form filling

--- newsApp/src/app/register/register.component.ts
+++ newsApp/src/app/register/register.component.ts
@@ -10,16 +10,26 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   registerForm! :FormGroup;
-
+  passwrdMismatch = false
 
   constructor(private formBuilder:FormBuilder) { 
 
     this.registerForm = this.formBuilder.group({
-      
+      firstName :'',
+      lastName :'',
+      email :'',
+      passwrd :'',
+      cfPasswrd :'',
     })
   }
 
   ngOnInit(): void {
+  }
+  register(){
+    if(this.registerForm.value['passwrd'] !=  this.registerForm.value['cfPasswrd']) {
+      this.passwrdMismatch = true
+    }
+    console.log(this.registerForm.value['passwrd'])
   }
 
 }

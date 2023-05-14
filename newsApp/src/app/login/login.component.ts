1c5ed4f8cb8b8c718c85b466514ac4e15f172505
Riad REKAB <hr_rekab@esi.dz>
2023-05-14 22:07:11 +0200

    Added errors handling for the login function

--- newsApp/src/app/login/login.component.ts
+++ newsApp/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
@@ -37,7 +39,18 @@ export class LoginComponent implements OnInit {
       this.authService.login(this.loginForm.getRawValue()).subscribe(data=> {
         // Have to add token
         window.location.href = '/search-page'
-      })
+      },
+      (error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          console.log('Wrong passeword');
+        } else if(error.status ===404) {
+          console.log("Email not found")
+        }
+        else {
+          console.log('An error occurred:', error.error.message);
+        }
+      }
+    );
     }
     
   }

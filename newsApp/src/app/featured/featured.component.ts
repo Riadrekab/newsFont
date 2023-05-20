aa294d124c9cd3320489ca43226751f614182124
Riad REKAB <hr_rekab@esi.dz>
2023-05-20 15:17:55 +0200

    Added the token refresh functions

--- newsApp/src/app/featured/featured.component.ts
+++ newsApp/src/app/featured/featured.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { PreferecesService } from '../prefereces.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { DOCUMENT } from '@angular/common';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-featured',
@@ -13,9 +16,27 @@ export class FeaturedComponent implements OnInit {
   full = true
 
 
-  constructor(private preferenceService: PreferecesService) {}
+  constructor(private preferenceService: PreferecesService,@Inject(DOCUMENT) private document: Document, private authService : AuthService) {}
 
   ngOnInit(): void {
+    this.authService.checkToken().subscribe(data => {
+      this.authService.refreshToken().subscribe(data=> {
+        this.document.cookie = `token=${(data as any).access }; path=/`;
+        this.document.cookie = `refresh=${(data as any).refresh}; path=/`;
+      })
+    },
+      (error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+          document.cookie = 'refresh=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+
+
+          if(this.isCookieDeleted('token') && this.isCookieDeleted('refresh')) {
+            window.location.href='/'
+          }
+        }
+      }
+    );
     this.preferenceService.getPrefered().subscribe(data => {
         let vals =JSON.parse(data.toString())
         this.listeTest = vals
@@ -27,6 +48,23 @@ export class FeaturedComponent implements OnInit {
 
   navigateToUrl(url: string) {
     window.open(url, '_blank');
+  }
+
+  isCookieDeleted(value:any) {
+    const cookies = document.cookie.split(';');
+    
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      
+      // Check if the cookie starts with the provided name
+      if (cookie.startsWith(`${value}=`)) {
+        // Cookie still exists
+        return false;
+      }
+    }
+    
+    // Cookie is deleted
+    return true;
   }
 
 
--- newsApp/src/app/history/history.component.ts
+++ newsApp/src/app/history/history.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-history',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryComponent implements OnInit {
   listTest = [1,2,3,4]
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document, private authService : AuthService) { }
 
   ngOnInit(): void {
+    this.authService.checkToken().subscribe(data => {
+      this.authService.refreshToken().subscribe(data=> {
+        this.document.cookie = `token=${(data as any).access }; path=/`;
+        this.document.cookie = `refresh=${(data as any).refresh}; path=/`;
+      })
+    },
+      (error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+          document.cookie = 'refresh=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+
+
+          if(this.isCookieDeleted('token') && this.isCookieDeleted('refresh')) {
+            window.location.href='/'
+          }
+        }
+      }
+    );
+  }
+
+  isCookieDeleted(value:any) {
+    const cookies = document.cookie.split(';');
+    
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      
+      // Check if the cookie starts with the provided name
+      if (cookie.startsWith(`${value}=`)) {
+        // Cookie still exists
+        return false;
+      }
+    }
+    
+    // Cookie is deleted
+    return true;
   }
 
 }
